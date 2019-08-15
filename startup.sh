0f70f5299b494549b57024acac6a06802a3b08f9
Adithya Philip <adithyaphilip@gmail.com>
2019-08-15 01:59:13 -0400

    sudo it all

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y iperf3
 sudo apt-get install -y python3-pip
-pip3 install netifaces
+sudo pip3 install netifaces
 
-python3 main.py > main_out 2> main_err
+sudo python3 main.py > main_out 2> main_err
