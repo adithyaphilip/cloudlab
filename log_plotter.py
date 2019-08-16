c1d5e7f0d42aeb3f46ef475b38093563eba3e5a2
Adithya Philip <adithyaphilip@gmail.com>
2019-08-16 05:18:51 -0400

    t

--- log_plotter.py
+++ log_plotter.py
@@ -47,25 +47,36 @@ def plot_hist(agg_bw_series: pd.DataFrame, cumulative: bool):
     fig.show()
 
 
+def plot_link_utilization(df: pd.DataFrame, bin_size_s: int):
+    df['bin'] = df.apply(lambda row: row['endtime'] // bin_size_s, axis=1)
+    bw_series = df.groupby(by="bin").apply(lambda rows: rows['datasize'].sum() / bin_size_s)
+    fig = px.line(pd.DataFrame({'Bins': bw_series.index, 'Bandwidth': bw_series.values}),
+                  x="Bins", y="Bandwidth", title='Link utilization')
+    fig.update_xaxes(title_text='Bins (%ds)' % bin_size_s)
+    fig.show()
+
+
 def main():
-    df = pd.read_csv('iperf3_log_parsed_merged_200',
+    df = pd.read_csv('iperf3_log_parsed_merged_100',
                      names=['ip', 'socket', 'endtime', 'datasize', 'interval', 'bw'])
     df['endtime'] = df['endtime'] - df['endtime'].min() + 1
 
-    df_partial = df[df['ip'] == '192.168.1.1']
-    plot_line_graph(df_partial)
-    df_partial = df[df['ip'] == '192.168.1.2']
-    plot_line_graph(df_partial)
-    df_partial = df[df['ip'] == '192.168.1.3']
-    plot_line_graph(df_partial)
-    df_partial = df[df['ip'] == '192.168.1.4']
-    plot_line_graph(df_partial)
-    df_partial = df[df['ip'] == '192.168.1.5']
-    plot_line_graph(df_partial)
+    # df_partial = df[df['ip'] == '192.168.1.1']
+    # plot_line_graph(df_partial)
+    # df_partial = df[df['ip'] == '192.168.1.2']
+    # plot_line_graph(df_partial)
+    # df_partial = df[df['ip'] == '192.168.1.3']
+    # plot_line_graph(df_partial)
+    # df_partial = df[df['ip'] == '192.168.1.4']
+    # plot_line_graph(df_partial)
+    # df_partial = df[df['ip'] == '192.168.1.5']
+    # plot_line_graph(df_partial)
 
-    df = trim_flow_times(60, 60, df)
-    plot_hist(get_avg_bw(df), False)
-    plot_hist(get_avg_bw(df), True)
+    plot_link_utilization(df, 60)
+
+    # df = trim_flow_times(60, 60, df)
+    # plot_hist(get_avg_bw(df), False)
+    # plot_hist(get_avg_bw(df), True)
     pass
 
 
