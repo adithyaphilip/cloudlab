ca44c742b4e85d76f3a36438b5185ba2ed43312e
Adithya Philip <adithyaphilip@gmail.com>
2019-08-16 05:43:25 -0400

    t

--- consts.py
+++ consts.py
@@ -15,7 +15,7 @@ PING_TRIES = 600 if MODE == Mode.PROD else 10
 PING_TIMEOUT_S = 5 if MODE == Mode.PROD else 1
 
 IP_PREFIX = "192.168.1." if MODE == Mode.PROD else "127.0.0."
-N_FLOWS = 128 if MODE == Mode.PROD else 2
+N_FLOWS = 100 if MODE == Mode.PROD else 2
 TEST_TIME_S = 600
 IPERF_LOG_INTERVAL_S = 5
 COUNT_PER_SIDE = 5
