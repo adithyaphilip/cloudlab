abba8f9457a0a5df3c57336a33477534470c0772
Adithya Philip <adithyaphilip@gmail.com>
2019-08-16 03:00:18 -0400

    t

--- rerun_exp.sh
+++ rerun_exp.sh
@@ -1,2 +1,4 @@
+
+parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h hosts_file_pssh "for pid in \$(ps aux | grep -e [i]perf3 | awk '{print \$2}'); do kill -9 \$pid; done;"
 parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h hosts_file_pssh "cd cloudlab; git pull; sudo python3 main.py &"
 # 'cd cloudlab && git pull && sudo python3 main.py 2> main_err > main_out'
