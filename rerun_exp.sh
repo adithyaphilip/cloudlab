2f4d6d8da601b28db769ac7f1e5d1d0f220377eb
aphilip <aphilip@node-1.aphilip-qv56693.dna-pg0.utah.cloudlab.us>
2019-08-16 00:38:07 -0600

    fixed

--- rerun_exp.sh
+++ rerun_exp.sh
@@ -1,1 +1,2 @@
-parallel-ssh -x "-oStrictHostKeyChecking=no  -i /home/ccc/xxx/something-us-east.pem" -h hosts_file_pssh 'cd cloudlab && git pull && sudo python3 main.py 2> main_err > main_out'
+parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h hosts_file_pssh "cd cloudlab; git pull; sudo python3 main.py"
+# 'cd cloudlab && git pull && sudo python3 main.py 2> main_err > main_out'
