82ab4cb0657d30571074628b94a23080d6ce2f6c
Adithya Philip <adithyaphilip@gmail.com>
2019-08-15 22:32:59 -0400

    cubic

--- left_hand.py
+++ left_hand.py
@@ -7,7 +7,7 @@ import consts
 # starts given number of flows targeted at given ip address incrementing port numbers by 1 starting at the given base
 def start_iperf(target_ip: str, n_flows: int, time_s: int):
     # NOTE: DON't FORGET TO PUT IN -C cubic!!!
-    subprocess.call("%s -c %s -P %d -t %d -i 1 --json > %s"
+    subprocess.call("%s -c %s -P %d -t %d -i 1 --json -C cubic > %s"
                     % (consts.IPERF_BINARY, target_ip, n_flows, time_s, consts.LOG_FILEPATH),
                     shell=True)
 
