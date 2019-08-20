7e301aebc8e6b9930f4ebd527de29712750adc45
Adithya Philip <adithyaphilip@gmail.com>
2019-08-20 03:21:23 -0400

    added check for ensuring git credentials are entered at start of experiment

--- rerun_exp.sh
+++ rerun_exp.sh
@@ -4,6 +4,9 @@ if [ -z "$1" ]
     exit 1
 fi
 
+# just to ensure the credential store has our password
+git checkout -b temp_verify && git push -fu origin temp_verify
+
 parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h hosts_file_pssh "for pid in \$(ps aux | grep -e [i]perf3 | awk '{print \$2}'); do kill -9 \$pid; done;"
 parallel-ssh -t 0 -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h hosts_file_pssh "cd cloudlab; git pull; sudo bash startup.sh; sudo python3 main.py $1 2>&1 | sudo tee main_combined.out"
 # 'cd cloudlab && git pull && sudo python3 main.py 2> main_err > main_out'
