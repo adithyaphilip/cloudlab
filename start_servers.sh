fcb971da3d1823a5b89167ab1454e5537270523e
Adithya Abraham Philip <aphilip@Adithyas-MacBook-Pro.local>
2019-10-08 01:38:35 -0400

    temp changing of server script

--- start_servers.sh
+++ start_servers.sh
@@ -1,5 +1,5 @@
 NUM_SERVERS_PER_NODE=100
-NETEM_DELAY_MS=20
+NETEM_DELAY_MS=0
 SERVER_LIST_FILE=servers_file_pssh
 BASE_PORT=6000
 
@@ -8,7 +8,8 @@ cat $SERVER_LIST_FILE
 
 echo "Setting Netem delay on servers to $NETEM_DELAY_MS ms"
 parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h  $SERVER_LIST_FILE \
-"sudo tc qdisc del dev eno50 root; sudo tc qdisc add dev eno50 root netem delay $NETEM_DELAY_MS""ms;"
+"sudo tc qdisc del dev eno50 root;"
+# sudo tc qdisc add dev eno50 root netem delay $NETEM_DELAY_MS""ms;
 echo "Killing existing iperf processes on servers"
 parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $SERVER_LIST_FILE \
 "for pid in \$(ps aux | grep -e [i]perf3 | awk '{print \$2}'); do sudo kill -9 \$pid; done;"
