c3be229239ee5a8e6b15cc4b17622a4c1c3f6a66
Adithya Abraham Philip <aphilip@Adithyas-MacBook-Pro.local>
2019-10-09 04:28:56 -0400

    run exp in reverse mode

--- rerun_exp.sh
+++ rerun_exp.sh
@@ -42,9 +42,10 @@ echo "Killing existing iPerf processes on clients"
 parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $CLIENT_TOT_PSSH_FILE \
 "for pid in \$(ps aux | grep -e [i]perf3 | awk '{print \$2}'); do sudo kill -9 \$pid; done;"
 
-echo "Running experiments on clients at $(TZ=EST5EDT date)"
+echo "Updating clients to latest git repo at $(TZ=EST5EDT date)"
 parallel-ssh -t 0 -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $CLIENT_PSSH_FILE \
 "cd cloudlab; git pull; bash startup.sh;"
+echo "Running experiments on clients at $(TZ=EST5EDT date)"
 parallel-ssh -t 0 -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $CLIENT_PSSH_FILE \
 "sudo python3 main.py $1 $2 $3 2>&1 | sudo tee main_combined.out"
 # 'cd cloudlab && git pull && sudo python3 main.py 2> main_err > main_out'
