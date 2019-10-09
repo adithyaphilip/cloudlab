a1741db7fd2ffdef78a71c04d35a26def0b949bc
Adithya Abraham Philip <aphilip@Adithyas-MacBook-Pro.local>
2019-10-09 04:34:00 -0400

    run exp in reverse mode

--- rerun_exp.sh
+++ rerun_exp.sh
@@ -46,8 +46,7 @@ echo "Updating clients to latest git repo at $(TZ=EST5EDT date)"
 parallel-ssh -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $CLIENT_PSSH_FILE \
 "cd cloudlab; git pull; bash startup.sh;"
 echo "Running experiments on clients at $(TZ=EST5EDT date)"
-parallel-ssh -t 0 -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $CLIENT_PSSH_FILE \
-"sudo python3 main.py $1 $2 $3 2>&1 | sudo tee main_combined.out"
+parallel-ssh -t 0 -x "-o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" -h $CLIENT_PSSH_FILE "sudo python3 main.py $1 $2 $3 2>&1 | sudo tee main_combined.out"
 # 'cd cloudlab && git pull && sudo python3 main.py 2> main_err > main_out'
 echo "Merging results from clients"
 cat iperf3_log_parsed* > iperf3_log_parsed_merged
