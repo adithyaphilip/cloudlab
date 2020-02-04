ac0ddd053a8a2d70d89127b33a512f26d229e8e7
Adithya Philip <adithyaphilip@gmail.com>
2020-02-04 05:42:19 -0500

    made variance guy multicore

--- offline/variance_checker.py
+++ offline/variance_checker.py
@@ -5,13 +5,18 @@ import sys
 import multiprocessing as mp
 
 BTL_LINK = 1280  # mega BYTES
+THREADS = 12
 
 
-def calc_jfi(df, )
+def calc_jfi(df):
+    df = log_plotter.trim_flow_times(log_plotter.TIME_S_TRIM, log_plotter.TIME_S_TRIM, df)
+    bws = log_plotter.get_avg_gprs(df, False)
+    return metric_calc.get_jfi(bws)
+
 
 def main():
-    nodes_flows_per_node_time_algo_basertt_type2rtt_type2count_trial_l = [(2, 6000, 600, 'cubic', 20, 0, 0, trial)
-                                                                          for trial in range(1, 50)]
+    nodes_flows_per_node_time_algo_basertt_type2rtt_type2count_trial_l = [(15, 4000, 600, 'cubic', 20, 0, 0, trial)
+                                                                          for trial in range(1, 51)]
     # (15, 800, 600, 'cubic', 20, 0, 0, 50),
     # (10, 6000, 600, 'cubic', 20, 0, 0, 50),
     # (15, 4000, 600, 'cubic', 20, 0, 0, 50),
@@ -21,16 +26,12 @@ def main():
     dfs_demands = log_plotter.get_dfs_and_demands(BTL_LINK,
                                                   nodes_flows_per_node_time_algo_basertt_type2rtt_type2count_trial_l)
 
-    jfis = mp.Array('d', len(dfs_demands))
-    for df, _ in dfs_demands:
-        df = log_plotter.trim_flow_times(log_plotter.TIME_S_TRIM, log_plotter.TIME_S_TRIM, df)
-        bws = log_plotter.get_avg_gprs(df, False)
-        jfi = metric_calc.get_jfi(bws)
-        jfis.append(jfi)
+    pool = mp.Pool(THREADS)
+    jfis = pool.map(calc_jfi, list(map(lambda x: x[0], dfs_demands)))
 
     print("Finished!")
 
-    with open("jfis.out", "w") as f:
+    with open('../results/jfis/%d_nodes_%d_flows_%d_s_%s_algo_rev_%d_nm1_%d_nm2_%d_delayed_%d' % nodes_flows_per_node_time_algo_basertt_type2rtt_type2count_trial_l[0], "w") as f:
         f.writelines(map(lambda x: str(x) + '\n', jfis))
 
 
