bd376343dfc79d6106f6072e008b2b95c9078945
Adithya Philip <adithyaphilip@gmail.com>
2020-10-27 07:18:20 -0400

    iperf install from git fixes

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,7 @@ parallel-ssh -h setup_cl_clients -t 0 'sudo rm -Rf cloudlab && git clone https:/
 
 # 2. Install iperf3 onto the nodes
 # WARNING: This is just intended to work with UBUNTU 18.04 on the xl170 nodes
-parallel-ssh $PSSH_OPTIONS -h setup_cl_clients "sudo apt remove -y iperf3; rm-rf iperf; git clone https://github.com/adithyaphilip/iperf.git && cd iperf && ./bootstrap.sh && ./configure && make && cd .. && sudo rm -f /usr/local/bin/iperf3 /usr/bin/iperf3 && sudo ln -s $(pwd)/iperf/src/iperf3 /usr/local/bin/iperf3 && sudo ln -s $(pwd)/iperf/src/iperf3 /usr/bin/iperf3"
+parallel-ssh $PSSH_OPTIONS -h setup_cl_clients "sudo apt remove -y iperf3; rm -rf iperf; git clone https://github.com/adithyaphilip/iperf.git && cd iperf && ./bootstrap.sh && ./configure && make && cd .. && sudo rm -f /usr/local/bin/iperf3 /usr/bin/iperf3 && sudo ln -s $(pwd)/iperf/src/iperf3 /usr/local/bin/iperf3 && sudo ln -s $(pwd)/iperf/src/iperf3 /usr/bin/iperf3"
 
 # 3. If iperf3 runs without this package, no worries if it fails
 # NOTE: lib32cz1 is required because of this bug (personally observed on Ubuntu 18.04): https://github.com/esnet/iperf/issues/168
